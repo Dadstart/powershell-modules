aecbf10b655d9457f52145a0253e8d9da750c6b3
Andrew Bishop <andrew@bishop4.com>
2025-07-23 09:33:49 -0500

    Renaming MediaTrack to MediaStream

---
+++ Modules/NewMedia/Classes/MediaStream.ps1
@@ -0,0 +1,20 @@
+class MediaStream {
+    [string] $FileName
+    [int] $
+    [psobject] $Raw
+
+    MediaStream([psobject]$stream) {
+        $this.Index = $stream.index
+        $this.Type = $stream.codec_type
+        $this.Codec = $stream.codec_name
+        $this.Language = $stream.tags.language
+        $this.Bitrate = [int]$stream.bit_rate
+        $this.Title = $stream.tags.title
+        $this.Duration = [TimeSpan]::FromSeconds([double]$stream.duration)
+        $this.Raw = $stream
+    }
+
+    [string] ToString() {
+        return "$($this.Type) Stream $($this.Index): $($this.Codec) [$($this.Language)]"
+    }
+}
